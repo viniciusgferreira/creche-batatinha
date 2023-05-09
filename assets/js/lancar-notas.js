e846fbcba07435ffc46b30bf3e6a3c3d14a5c61c
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-05-09 10:16:19 -0300

    fix filled paw size

--- assets/js/lancar-notas.js
+++ assets/js/lancar-notas.js
@@ -5,37 +5,37 @@
 document.addEventListener('DOMContentLoaded', loadCat)
 
 function getParamID() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const id = urlParams.get('id')
-    return id
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const id = urlParams.get('id')
+  return id
 
 }
 
 async function loadCat() {
-    const catID = getParamID()
-    const cat = await fetchCatByID(catID)
-    renderDate()
-    renderCat(cat)
+  const catID = getParamID()
+  const cat = await fetchCatByID(catID)
+  renderDate()
+  renderCat(cat)
 }
 
 async function fetchCatByID(id) {
-    const url = `https://644a8480a8370fb321510499.mockapi.io/api/v1/gatos/${id}`;
+  const url = `https://644a8480a8370fb321510499.mockapi.io/api/v1/gatos/${id}`;
 
-    try {
-        const response = await fetch(url)
-        const cat = await response.json()
-        return cat
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const response = await fetch(url)
+    const cat = await response.json()
+    return cat
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 function renderDate() {
-    const dataElemento = document.querySelector('#data')
-    const month = new Date().toLocaleString('default', { month: 'long' });
-    const year = new Date().toLocaleString('default', { year: 'numeric' });
-    dataElemento.textContent = month.charAt(0).toUpperCase() + month.slice(1) + '/' + year;
+  const dataElemento = document.querySelector('#data')
+  const month = new Date().toLocaleString('default', { month: 'long' });
+  const year = new Date().toLocaleString('default', { year: 'numeric' });
+  dataElemento.textContent = month.charAt(0).toUpperCase() + month.slice(1) + '/' + year;
 }
 
 /*
@@ -77,95 +77,94 @@ document.addEventListener('DOMContentLoaded', function () {
 let notaAtual = 0;
 
 function renderCat(cat) {
-    const catNameEl = document.querySelector('#nomeGato')
-    const catPhotoEl = document.querySelector('#fotoGato')
+  const catNameEl = document.querySelector('#nomeGato')
+  const catPhotoEl = document.querySelector('#fotoGato')
 
-    catNameEl.textContent = cat.nomeGato
-    catPhotoEl.src = cat.fotoGato
+  catNameEl.textContent = cat.nomeGato
+  catPhotoEl.src = cat.fotoGato
 
-    for (const category in cat.notas) {
-        const div = document.querySelector(`#${category}`);
-        let filledPawCounter = 0;
-        notaAtual = cat.notas[category];
-        for (let i = 0; i < 5; i++) {
-            if (filledPawCounter < cat.notas[category]) {
-                div.appendChild(createFilledPaw());
-                filledPawCounter++;
-            } else {
-                div.appendChild(createEmptyPaw())
-            }
-        }
+  for (const category in cat.notas) {
+    const div = document.querySelector(`#${category}`);
+    let filledPawCounter = 0;
+    notaAtual = cat.notas[category];
+    for (let i = 0; i < 5; i++) {
+      if (filledPawCounter < cat.notas[category]) {
+        div.appendChild(createFilledPaw());
+        filledPawCounter++;
+      } else {
+        div.appendChild(createEmptyPaw())
+      }
     }
+  }
 }
 
 function createFilledPaw() {
-    const filledPaw = document.createElement('img')
-    filledPaw.src = 'https://i.postimg.cc/hX9g5d5g/patinha.png'
-    //'https://img.icons8.com/ios-filled/50/null/cat-footprint.png'
-    filledPaw.classList.add('iconePata--preenchida')
-    filledPaw.addEventListener('click', function() {
-        mudarPatinha(filledPaw)});
-    return filledPaw;
+  const filledPaw = document.createElement('img')
+  filledPaw.src = 'https://i.postimg.cc/hX9g5d5g/patinha.png'
+  filledPaw.classList.add('iconePata--preenchida', 'iconePata')
+  filledPaw.addEventListener('click', function () {
+    mudarPatinha(filledPaw)
+  });
+  return filledPaw;
 }
 
 function createEmptyPaw() {
-    const emptyPaw = document.createElement('img')
-    emptyPaw.src = 'https://i.postimg.cc/G40dKQzq/patinha-1.png'
-    emptyPaw.classList.add('iconePata');
-    
-    emptyPaw.addEventListener('click', function() {
-        mudarPatinha(emptyPaw);
-    });
-    return emptyPaw
+  const emptyPaw = document.createElement('img')
+  emptyPaw.src = 'https://i.postimg.cc/G40dKQzq/patinha-1.png'
+  emptyPaw.classList.add('iconePata');
+
+  emptyPaw.addEventListener('click', function () {
+    mudarPatinha(emptyPaw);
+  });
+  return emptyPaw
 }
 
 function mudarPatinha(paw) {
-    if (paw.src.endsWith('patinha-1.png')) {
-        paw.src = 'https://i.postimg.cc/hX9g5d5g/patinha.png';
-        filledPawCounter++;
-        updateCat(cat);
+  if (paw.src.endsWith('patinha-1.png')) {
+    paw.src = 'https://i.postimg.cc/hX9g5d5g/patinha.png';
+    filledPawCounter++;
+    updateCat(cat);
 
-        // aumentar nota
-    } else if (paw.src.endsWith('patinha.png')) {
-        paw.src = 'https://i.postimg.cc/G40dKQzq/patinha-1.png';
-        filledPawCounter--;
-        updateCat(cat);
-        // diminuir nota
-    }
-    
+    // aumentar nota
+  } else if (paw.src.endsWith('patinha.png')) {
+    paw.src = 'https://i.postimg.cc/G40dKQzq/patinha-1.png';
+    filledPawCounter--;
+    updateCat(cat);
+    // diminuir nota
+  }
+
 }
 
 async function submitForm() {
-    const catID = getParamID();
-    const cat = await fetchCatByID(catID);
-    for (const category in cat.notas) {
-      const div = document.querySelector(`#${category}`);
-      const filledPaws = div.querySelectorAll('.iconePata--preenchida');
-      cat.notas[category] = filledPaws.length;
-    }
-    
-    await updateCat(cat);
-  }
-  
-  async function updateCat(cat) {
-    const url = `https://644a8480a8370fb321510499.mockapi.io/api/v1/gatos/${cat.id}`;
-    const options = {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(cat)
-    };
-    try {
-      const response = await fetch(url, options);
-      const updatedCat = await response.json();
-      console.log(updatedCat);
-      console.log(response.status); 
-    } catch (error) {
-      console.log(error);
-    }
+  const catID = getParamID();
+  const cat = await fetchCatByID(catID);
+  for (const category in cat.notas) {
+    const div = document.querySelector(`#${category}`);
+    const filledPaws = div.querySelectorAll('.iconePata--preenchida');
+    cat.notas[category] = filledPaws.length;
   }
 
-  document.querySelector('#botaoSalvarNotas').onclick = submitForm;
-  
-  
+  await updateCat(cat);
+}
+
+async function updateCat(cat) {
+  const url = `https://644a8480a8370fb321510499.mockapi.io/api/v1/gatos/${cat.id}`;
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(cat)
+  };
+  try {
+    const response = await fetch(url, options);
+    const updatedCat = await response.json();
+    console.log(updatedCat);
+    console.log(response.status);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+document.querySelector('#botaoSalvarNotas').onclick = submitForm;
+
