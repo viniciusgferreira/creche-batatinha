265a5f09074b147634d0ebfc3524456754643602
chrixtianny <christianny@live.com>
2023-05-09 04:07:46 -0300

    css, novas funções

--- assets/js/lancar-notas.js
+++ assets/js/lancar-notas.js
@@ -38,6 +38,44 @@ function renderDate() {
     dataElemento.textContent = month.charAt(0).toUpperCase() + month.slice(1) + '/' + year;
 }
 
+/*
+document.addEventListener('DOMContentLoaded', function () {
+    let elementos = [
+      document.getElementById('socializacao'),
+      document.getElementById('sonequinha'),
+      document.getElementById('alimentacao'),
+      document.getElementById('brincadeiras'),
+      document.getElementById('preguicinha'),
+      document.getElementById('fotos')
+    ];
+  
+    let botao = document.getElementById('botaoSalvarNotas');
+    botao.disabled = true;
+  
+    function validarInputs() {
+      let temNota = false;
+      for (const category in cat.notas) {
+        if (filledPawCounter >= 1){
+            temNota = true;
+            break;
+        }
+      }
+      if (temNota) {
+        botao.classList.remove('botaoSalvar--desabilitado');
+        botao.disabled = false;
+      } else {
+        botao.classList.add('botaoSalvar--desabilitado');
+        botao.disabled = true;
+      }
+    }
+  
+    for (let i = 0; i < elementos.length; i++) {
+      elementos[i].addEventListener('input', validarInputs);
+    }
+  });
+  */
+let notaAtual = 0;
+
 function renderCat(cat) {
     const catNameEl = document.querySelector('#nomeGato')
     const catPhotoEl = document.querySelector('#fotoGato')
@@ -48,30 +86,86 @@ function renderCat(cat) {
     for (const category in cat.notas) {
         const div = document.querySelector(`#${category}`);
         let filledPawCounter = 0;
+        notaAtual = cat.notas[category];
         for (let i = 0; i < 5; i++) {
             if (filledPawCounter < cat.notas[category]) {
-                div.appendChild(createPawFilled());
+                div.appendChild(createFilledPaw());
                 filledPawCounter++;
             } else {
-                div.appendChild(createPawEmpty())
+                div.appendChild(createEmptyPaw())
             }
         }
     }
 }
 
-function createPawFilled() {
+function createFilledPaw() {
     const filledPaw = document.createElement('img')
-    filledPaw.src = 'https://img.icons8.com/ios-filled/50/null/cat-footprint.png'
-    filledPaw.classList.add('iconePata')
+    filledPaw.src = 'https://i.postimg.cc/hX9g5d5g/patinha.png'
+    //'https://img.icons8.com/ios-filled/50/null/cat-footprint.png'
+    filledPaw.classList.add('iconePata--preenchida')
+    filledPaw.addEventListener('click', function() {
+        mudarPatinha(filledPaw)});
     return filledPaw;
 }
 
-function createPawEmpty() {
+function createEmptyPaw() {
     const emptyPaw = document.createElement('img')
-    emptyPaw.src = 'https://img.icons8.com/ios/50/000000/cat-footprint.png'
-    emptyPaw.classList.add('iconePata')
+    emptyPaw.src = 'https://i.postimg.cc/G40dKQzq/patinha-1.png'
+    emptyPaw.classList.add('iconePata');
+    
+    emptyPaw.addEventListener('click', function() {
+        mudarPatinha(emptyPaw);
+    });
     return emptyPaw
 }
 
-function lancarNotas() {
+function mudarPatinha(paw) {
+    if (paw.src.endsWith('patinha-1.png')) {
+        paw.src = 'https://i.postimg.cc/hX9g5d5g/patinha.png';
+        filledPawCounter++;
+        updateCat(cat);
+
+        // aumentar nota
+    } else if (paw.src.endsWith('patinha.png')) {
+        paw.src = 'https://i.postimg.cc/G40dKQzq/patinha-1.png';
+        filledPawCounter--;
+        updateCat(cat);
+        // diminuir nota
+    }
+    
 }
+
+async function submitForm() {
+    const catID = getParamID();
+    const cat = await fetchCatByID(catID);
+    for (const category in cat.notas) {
+      const div = document.querySelector(`#${category}`);
+      const filledPaws = div.querySelectorAll('.iconePata--preenchida');
+      cat.notas[category] = filledPaws.length;
+    }
+    
+    await updateCat(cat);
+  }
+  
+  async function updateCat(cat) {
+    const url = `https://644a8480a8370fb321510499.mockapi.io/api/v1/gatos/${cat.id}`;
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(cat)
+    };
+    try {
+      const response = await fetch(url, options);
+      const updatedCat = await response.json();
+      console.log(updatedCat);
+      console.log(response.status); 
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  document.querySelector('#botaoSalvarNotas').onclick = submitForm;
+  
+  
